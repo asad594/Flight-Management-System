795acccf05760abc07236b04ca0ae5ac3e36383d
Muhammad Asad <asad.spartan300@gmail.com>
2026-08-13 02:02:23 +0500

    docs(asgi): document ASGI application entrypoint for async server deployment in flight_system/asgi.py

--- flight_system/asgi.py
+++ flight_system/asgi.py
@@ -1,3 +1,8 @@
+"""
+ASGI config for SkyBound Flight Management System.
+Exposes the ASGI callable as a module-level variable named `application`.
+"""
+
 import os
 from django.core.asgi import get_asgi_application
 
