88b673b1313b2d2d3ae0eac9a827dd500e4ccbd2
Muhammad Asad <asad.spartan300@gmail.com>
2026-08-13 02:02:33 +0500

    docs(wsgi): document WSGI web application interface in flight_system/wsgi.py

--- flight_system/wsgi.py
+++ flight_system/wsgi.py
@@ -1,3 +1,8 @@
+"""
+WSGI config for SkyBound Flight Management System.
+Exposes the WSGI callable as a module-level variable named `application`.
+"""
+
 import os
 from django.core.wsgi import get_wsgi_application
 
