f2acf5bfb9321a8f1ed74f444753f3ef0039afe4
Muhammad Asad <asad.spartan300@gmail.com>
2026-08-14 14:08:16 +0500

    docs(apps): configure AppConfig metadata and verbose name in core/apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,14 @@
+"""
+SkyBound Core Application Configuration.
+Declares Django application registry settings and metadata for the core domain module.
+"""
+
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
+    """Configuration class for SkyBound core flight management application."""
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    verbose_name = 'SkyBound Flight Management'
+
